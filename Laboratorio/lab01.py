cf1858a9a1870880ccfdc062d97864c8d0a74d59
Nathaly Izawa <128828789+NLissaSI@users.noreply.github.com>
2024-07-17 15:09:53 -0300

    Create lab01.py

---
+++ Laboratorio/lab01.py
@@ -0,0 +1,16 @@
+######################################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 1 - Aritmética com Inteiros
+# Nome: Nathaly Lissa S. Izawa
+# RA: 186028
+######################################################################
+
+a = int(input())
+b = int(input())
+print("a =", a)
+print("b =", b)
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a // b =", a // b)
+print("a % b =", a % b)
